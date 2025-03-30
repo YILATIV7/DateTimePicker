54fe75533fe7d6fb125c73f00dc43aa8b501a222
Vitalii <vitalytyrenko7@gmail.com>
2025-03-30 18:02:59 +0300

    Auto-complete for days and months

--- src/app/date-time-picker/date-time-picker.controller.ts
+++ src/app/date-time-picker/date-time-picker.controller.ts
@@ -1,4 +1,3 @@
-
 export class DateTimePickerController {
 
     private numberPositions: number[];
@@ -54,65 +53,28 @@ export class DateTimePickerController {
                 value[pos] = ' ';
                 cursorPosition = pos;
             }
-        }
-
-        else if (event.inputType === "deleteContentForward") {
+        } else if (event.inputType === "deleteContentForward") {
             if (cursorPosition < value.length && !this.separatorPositions.includes(cursorPosition)) {
                 value[cursorPosition] = ' ';
             }
-        }
-
-        else if (event.inputType === "insertText") {
+        } else if (event.inputType === "insertText") {
             let inputData = event.data?.replace(/\D/g, "");
             if (!inputData || cursorPosition === this.value.length) return;
 
-            /* Base logic */
-            // let pos = this.separatorPositions.includes(cursorPosition) ? cursorPosition + 1 : cursorPosition;
-            // value[pos] = inputData[0];
-            // cursorPosition = pos + 1;
-
-            /* Date logic */
             if (this.separatorPositions.includes(cursorPosition)) cursorPosition += 1;
+            const result = this.applyAutoComplete(value, cursorPosition, inputData[0]);
 
-            const digit = parseInt(inputData[0])
-
-            if (this.type.includes('date')) {
-
-                if ((cursorPosition === 0 && digit > 3) || (cursorPosition === 3 && digit > 1)) {
-                    value[cursorPosition] = '0';
-                    value[cursorPosition + 1] = digit+'';
-                    cursorPosition += 3;
-                }
-                else if (cursorPosition === 1 && value[0] === '3' && digit > 1) {
-                    value[0] = '0';
-                    value[1] = '3';
-                    value[3] = '0';
-                    value[4] = digit+'';
-                    cursorPosition = 6;
-                }
-                else if (cursorPosition === 4 && value[3] === '1' && digit > 2) {
-                    value[4] = '2';
-                    cursorPosition = 6;
-                }
-                else {
-                    value[cursorPosition] = digit+'';
-                    cursorPosition += 1;
-                    if (this.separatorPositions.includes(cursorPosition)) cursorPosition += 1;
-                }
+            if (result.cursorPosition === cursorPosition) {
+                value[cursorPosition] = inputData[0];
+                cursorPosition += 1;
+                if (this.separatorPositions.includes(cursorPosition)) cursorPosition += 1;
+            } else {
+                value = result.value;
+                cursorPosition = result.cursorPosition;
             }
-
-            /* Hours and minutes logic */
-            if (this.type.includes('time')) {
-                let timePosition = this.type === 'date-time' ? 11 : 0;
-
-            }
-        }
-
-        else if (event.inputType === "insertFromPaste") {
+        } else if (event.inputType === "insertFromPaste") {
             // TODO:
-        }
-
-        else if (event.inputType === 'deleteByCut') {
+        } else if (event.inputType === 'deleteByCut') {
             // TODO:
         }
 
@@ -127,5 +89,54 @@ export class DateTimePickerController {
 
     public onClick(event: MouseEvent) {
 
+    }
+
+    private applyAutoComplete(value: string[], cursorPosition: number, char: string) {
+        const digit = parseInt(char);
+        value = [...value];
+
+        if (this.type.includes('date')) {
+            if ((cursorPosition === 0 && digit > 3) || (cursorPosition === 3 && digit > 1)) {
+                value[cursorPosition] = '0';
+                value[cursorPosition + 1] = digit + '';
+                cursorPosition += 3;
+            } else if (cursorPosition === 1 && value[0] === '3' && digit > 1) {
+                value[0] = '0';
+                value[1] = '3';
+                value[3] = '0';
+                value[4] = digit + '';
+                cursorPosition = 6;
+            } else if (cursorPosition === 4 && value[3] === '1' && digit > 2) {
+                value[4] = '2';
+                cursorPosition = 6;
+            }
+        }
+
+        if (this.type.includes('time')) {
+            let timePosition = this.type === 'date-time' ? 11 : 0;
+
+            if (cursorPosition === timePosition && digit > 2) {
+                value[timePosition] = '0';
+                value[timePosition + 1] = digit + '';
+                cursorPosition += 3;
+            } else if (cursorPosition === timePosition + 1 && value[timePosition] === '2' && digit > 5) {
+                value[timePosition] = '0';
+                value[timePosition + 1] = '2';
+                value[timePosition + 3] = '0';
+                value[timePosition + 4] = digit + '';
+                cursorPosition = timePosition + 5;
+            } else if (cursorPosition === timePosition + 1 && value[timePosition] === '2' && digit > 3) {
+                value[timePosition] = '0';
+                value[timePosition + 1] = '2';
+                value[timePosition + 3] = digit + '';
+                cursorPosition = timePosition + 4;
+            } else if (cursorPosition === timePosition + 3 && digit > 5) {
+                value[timePosition + 3] = '0';
+                value[timePosition + 4] = digit + '';
+                cursorPosition = timePosition + 5;
+            }
+        }
+
+        return {value, cursorPosition};
     }
 }
